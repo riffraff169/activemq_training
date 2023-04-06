47b19c8a39409d648235c77be3e7e3f8dfc3e46f
Lance Dillon <riffraff169@yahoo.com>
2023-04-06 14:06:42 -0400

    init submodules and build amqutil

--- build_amqutil.sh
+++ build_amqutil.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-cd amqutil
-docker run -it --rm --name amqutil -v "$(pwd)":/usr/src/maven -w /usr/src/maven maven mvn compile assembly:single
+git submodule init
+git submodule update
+
+if [ "$1" == "build" ]; then
+    # shellcheck disable=SC2164
+    cd amqutil
+    docker run -it --rm --name amqutil -v "$(pwd)":/usr/src/maven -w /usr/src/maven maven mvn compile assembly:single
+fi
 
